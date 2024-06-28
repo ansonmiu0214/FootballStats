f379c0dd722c5b76c80da9b4828b33167cbe336e
Anson Miu <me@ansonmiu.dev>
2024-06-28 09:22:40 +0000

    leaguelib: extract HTTP logic into separate class

--- flaskproject/logic/leaguelib.py
+++ flaskproject/logic/leaguelib.py
@@ -8,58 +8,61 @@ from typing import Any
 
 
 _SEASON = 2023
-_API_HOST = 'v3.football.api-sports.io'
-_API_HEADERS = {
-    'x-rapidapi-key': '46e3603952bbef534e2356d69f0a1ed6',   # TODO: remove!
-    'x-rapidapi-host': _API_HOST,
-    'season': _SEASON,
-}
 
 
-def _get_connection():
-    return http.client.HTTPSConnection(_API_HOST)
+class LeagueAPI:
+    def __init__(self):
+        self._conn = None
+        self._host = 'v3.football.api-sports.io'
+        self._headers = {
+            'x-rapidapi-key': '46e3603952bbef534e2356d69f0a1ed6',   # TODO: remove!
+            'x-rapidapi-host': self._host,
+            'season': _SEASON,
+        }
+
+    def __enter__(self):
+        self._conn = http.client.HTTPSConnection(self._host)
+        return self
+
+    def get(self, url: str):
+        """Fetch the specified 'url' and return the parsed JSON response."""
+
+        assert self._conn is not None
+
+        safe_url = urllib.parse.quote(url, safe='/?=')
+        self._conn.request('GET', safe_url, headers=self._headers)
+        res = self._conn.getresponse()
+        raw_data = res.read()
+
+        data = json.loads(raw_data.decode('utf-8'))
+        response = data['response']
+        return response
+
+    def __exit__(self):
+        self._conn = self._conn.close()
 
 
 def get_team(name: str) -> dict:
-    url = urllib.parse.quote(f'/teams?name={name}', safe='/?=')
+    with LeagueAPI() as api:
+        response = api.get(f'/teams?name={name}')
     
-    conn = _get_connection()
-    conn.request('GET', url, headers=_API_HEADERS)
-    res = conn.getresponse()
-    raw_data = res.read()
-
-    data = json.loads(raw_data.decode('utf-8'))
-    response = data['response']
     assert type(response) is list
     assert len(response) == 1
     return response[0]
 
 
 def get_teams_for_league(id: Any) -> list[dict]:
-    url = urllib.quote(f'/teams?league={id}&season={_SEASON}')
+    with LeagueAPI() as api:
+        response = api.get(f'/teams?league={id}&season={_SEASON}')
     
-    conn = _get_connection()
-    conn.request('GET', url, headers=_API_HEADERS)
-    res = conn.getresponse()
-    raw_data = res.read()
-
-    data = json.loads(raw_data.decode('utf-8'))
-    response = data['response']
     assert type(response) is list
-
     return response
 
 
 def get_players_for_team(id: Any) -> list[dict]:
-    url = urllib.quote(f'/players/squads?team={id}')
+    with LeagueAPI() as api:
+        response = api.get(f'/players/squads?team={id}')
     
-    conn = _get_connection()
-    conn.request('GET', url, headers=_API_HEADERS)
-    res = conn.getresponse()
-    raw_data = res.read()
-
-    data = json.loads(raw_data.decode('utf-8'))
-    response = data['response']
     assert type(response) is list
     assert len(response) == 1
 
